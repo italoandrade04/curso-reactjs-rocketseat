7db9a06d024d0c14b2c0f5010eae9cd5eaf2dca1
italoandrade04 <italoandrade04@hotmail.com>
2019-10-31 08:54:42 -0300

    Final project ReactJS - Rocketseat Course

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Routes from './routes';
+
+import Main from './pages/main';
+import Header from './components/Header';
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
